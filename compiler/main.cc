dde309eba55c5fcd9ef181f7e9ee959e529fe307
OtherCannon <>
2021-04-23 18:02:33 +0000

    Continued IR Integration

--- compiler/main.cc
+++ compiler/main.cc
@@ -219,6 +219,61 @@ Value *LogErrorV(const char *Str) {
 	return nullptr;
 }
 
+/// IR Codegen
+Value *NumberExprAST::codegen() {
+	return ConstantFP::get(TheContext, APFloat(Val));
+}
+
+Value *VariableExprAST::codegen() {
+	// Look up this variable name in the function.
+	Value *V = NamedValues[Name];
+	if (!V)
+		LogErrorV("Unknown variable name");
+	return V;
+}
+
+Value *BinaryExprAST::codegen() {
+	Value *L = LHS->codegen();
+	Value *R = RHS->codegen();
+	if (!L || !R)
+		return nullptr;
+	
+	switch (Op) {
+		case '+':
+			return Builder.CreateFAdd(L, R, "addtmp");
+		case '-':
+			return Builder.CreateFSub(L, R, "subtmp");
+		case '*':
+			return Builder.CreateFMul(L, R, "multmp");
+		case '<':
+			L = Builder.CreateFCmpULT(L, R, "cmptmp");
+			// Convert bool 0/1 to double 0.0 or 1.0
+			return Builder.CreateUIToFP(L, Type::getDoubleTy(TheContext), "booltmp");
+		default:
+			return LogErrorV("invalid binary operator");
+	}
+}
+
+Value *CallExprAST::codegen() {
+	// Look up the name in the global module table.
+	Function *CalleeF = TheModule->getFunction(Callee);
+	if (!CalleeF)
+		return LogErrorV("Unknown function referenced");
+	
+	// If argument mismatch error
+	if (Callee->arg_size() != Args.size())
+		return LogErrorV("Incorrect # arguments passed");
+	
+	std::vector<Value *> ArgsV;
+	for (unsigned i = 0, e = Args.size(); i != e; ++i) {
+		ArgsV.push_back(Args[i]->codegen());
+		if (!ArgsV.back())
+		return nullptr;
+	}
+
+	return Builder.CreateCall(CalleeF, ArgsV, "calltmp");
+}
+
 static std::unique_ptr<ExprAST> ParseExpression();
 
 /// numberexpr ::= number
